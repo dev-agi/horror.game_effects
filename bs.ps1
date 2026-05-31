14ad6c090446ccfffef8fc08ec01ad61fa7736b8
Agi <ensarakin2006@gmail.com>
2026-05-31 05:11:26 +0300

    Update bs.ps1

--- bs.ps1
+++ bs.ps1
@@ -3,36 +3,46 @@ $p = Get-Content "$PSScriptRoot\params_$($args[0]).json" | ConvertFrom-Json
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-$bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-
-$bmp = New-Object System.Drawing.Bitmap $bounds.Width, $bounds.Height
-$g = [System.Drawing.Graphics]::FromImage($bmp)
-$g.CopyFromScreen($bounds.Location, [System.Drawing.Point]::Empty, $bounds.Size)
-$g.Dispose()
-
 $form = New-Object System.Windows.Forms.Form
 $form.WindowState = 'Maximized'
 $form.FormBorderStyle = 'None'
 $form.TopMost = $true
 $form.ShowInTaskbar = $false
+$form.BackColor = "Black"
 
-$pic = New-Object System.Windows.Forms.PictureBox
-$pic.Dock = 'Fill'
-$pic.Image = $bmp
-$pic.SizeMode = 'StretchImage'
+$form.Add_Shown({
+    Start-Sleep -Milliseconds 150
 
-$form.Controls.Add($pic)
+    [System.Windows.Forms.Cursor]::Hide()
+
+    $bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+    $bmp = New-Object System.Drawing.Bitmap $bounds.Width, $bounds.Height
+    $g = [System.Drawing.Graphics]::FromImage($bmp)
+
+    $g.CopyFromScreen($bounds.Location, [System.Drawing.Point]::Empty, $bounds.Size)
+    $g.Dispose()
+
+    $pic = New-Object System.Windows.Forms.PictureBox
+    $pic.Dock = 'Fill'
+    $pic.Image = $bmp
+    $pic.SizeMode = 'StretchImage'
+
+    $form.Controls.Add($pic)
+})
 
 $timer = New-Object System.Windows.Forms.Timer
 $timer.Interval = [int]$p.Time * 1000
 
 $timer.Add_Tick({
     $timer.Stop()
+    [System.Windows.Forms.Cursor]::Show()
     $form.Close()
 })
 
-$form.Add_Shown({
-    $timer.Start()
+$form.Add_FormClosed({
+    [System.Windows.Forms.Cursor]::Show()
 })
+
+$timer.Start()
 
 [System.Windows.Forms.Application]::Run($form)
