80ee2250a6689e78717b8ae9f114cf050a50b159
Agi <ensarakin2006@gmail.com>
2026-05-31 04:43:26 +0300

    Update bs.ps1

--- bs.ps1
+++ bs.ps1
@@ -7,30 +7,80 @@ $form = New-Object System.Windows.Forms.Form
 $form.WindowState = 'Maximized'
 $form.FormBorderStyle = 'None'
 $form.TopMost = $true
-$form.BackColor = [System.Drawing.Color]::FromArgb(0, 0, 255)
+$form.BackColor = [System.Drawing.Color]::FromArgb(0, 120, 215)
+$form.Cursor = [System.Windows.Forms.Cursors]::Default
 
-$label = New-Object System.Windows.Forms.Label
-$label.ForeColor = [System.Drawing.Color]::White
-$label.BackColor = [System.Drawing.Color]::Transparent
-$label.AutoSize = $true
-$label.Font = New-Object System.Drawing.Font("Consolas", 18)
-$label.Text = "A problem has been detected and Windows has been shut down to prevent damage to your computer.`n`nIf this is the first time you've seen this Stop error screen, restart your computer.`n`nStop code: FAKE_BLUE_SCREEN"
+$fontMain = New-Object System.Drawing.Font("Segoe UI", 28)
+$fontSmall = New-Object System.Drawing.Font("Segoe UI", 14)
 
-$form.Controls.Add($label)
+$smiley = New-Object System.Windows.Forms.Label
+$smiley.Text = ":("
+$smiley.ForeColor = "White"
+$smiley.Font = New-Object System.Drawing.Font("Segoe UI", 72)
+$smiley.AutoSize = $true
+$smiley.Left = 120
+$smiley.Top = 80
 
-$form.Add_Shown({
-    $label.Left = 50
-    $label.Top = 50
-})
+$text = New-Object System.Windows.Forms.Label
+$text.ForeColor = "White"
+$text.Font = $fontMain
+$text.AutoSize = $true
+$text.Text = "Your PC ran into a problem and needs to restart."
+$text.Left = 120
+$text.Top = 200
+
+$sub = New-Object System.Windows.Forms.Label
+$sub.ForeColor = "White"
+$sub.Font = $fontSmall
+$sub.AutoSize = $true
+$sub.Text = "We're just collecting some error info, and then we'll restart for you."
+$sub.Left = 120
+$sub.Top = 270
+
+$progress = New-Object System.Windows.Forms.Label
+$progress.ForeColor = "White"
+$progress.Font = $fontSmall
+$progress.AutoSize = $true
+$progress.Text = "0% complete"
+$progress.Left = 120
+$progress.Top = 330
+
+$stop = New-Object System.Windows.Forms.Label
+$stop.ForeColor = "White"
+$stop.Font = $fontSmall
+$stop.AutoSize = $true
+$stop.Text = "If you'd like to know more, you can search online later for this error: CRITICAL_PROCESS_DIED"
+$stop.Left = 120
+$stop.Top = 420
+
+$qr = New-Object System.Windows.Forms.Label
+$qr.Text = "[ QR CODE ]"
+$qr.ForeColor = "White"
+$qr.Font = New-Object System.Drawing.Font("Consolas", 12)
+$qr.AutoSize = $true
+$qr.Left = 120
+$qr.Top = 520
+
+$form.Controls.AddRange(@($smiley,$text,$sub,$progress,$stop,$qr))
+
+$percent = 0
 
 $timer = New-Object System.Windows.Forms.Timer
-$timer.Interval = [int]$p.Time * 1000
+$timer.Interval = 100
 
 $timer.Add_Tick({
-    $timer.Stop()
-    $form.Close()
+    $percent += 1
+    if ($percent -gt 100) {
+        $timer.Stop()
+        $form.Close()
+    }
+    $progress.Text = "$percent% complete"
 })
 
 $timer.Start()
+
+Start-Sleep -Seconds ([int]$p.Time)
+
+$form.Close()
 
 [System.Windows.Forms.Application]::Run($form)
