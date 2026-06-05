9b1e59f27c6e473100281fb12b6ce7cca16e4ec6
Agi <ensarakin2006@gmail.com>
2026-06-06 00:44:06 +0300

    Update gp.ps1

--- gp.ps1
+++ gp.ps1
@@ -1,8 +1,14 @@
-$publicIP = (Invoke-RestMethod -Uri "https://ipecho.net/plain" -UseBasicParsing).Trim()
+$p = Get-Content "$PSScriptRoot\params_$($args[0]).json" | ConvertFrom-Json
+
+$localIP = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {
+    $_.InterfaceAlias -notmatch "Loopback" -and
+    $_.InterfaceAlias -notmatch "vEthernet" -and
+    $_.IPAddress -notmatch "^169\."
+} | Select-Object -First 1).IPAddress
 
 $response = @{
-    connectionId        = $p.connectionId
-    ip                  = $publicIP
+    connectionId = $p.connectionId
+    selected     = $localIP
 } | ConvertTo-Json -Compress
 
 $responsePath = Join-Path $PSScriptRoot "..\response_$($p.connectionId).json"
