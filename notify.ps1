efe3e5ffe36c24fcd17f9f04456f8d379458ae4f
Agi <ensarakin2006@gmail.com>
2026-06-07 17:16:01 +0300

    Update notify.ps1

--- notify.ps1
+++ notify.ps1
@@ -5,32 +5,82 @@ if ($uid) {
     $p = Get-Content "$PSScriptRoot\params_$uid.json" -ErrorAction SilentlyContinue | ConvertFrom-Json
 }
 
-$title = "HorrorGame"
-$text = "He is watching you.."
-$time = 10
+$title   = "HorrorGame"
+$text    = "He is watching you.."
+$time    = 10
+$iconUrl = "https://clipart-library.com/new_gallery/20-202034_eye-red-scary-vampire-redeyes-eyecolor-eyeball-iridology.png"
 
 if ($null -ne $p) {
-    if ($p.title) { $title = $p.title }
-    if ($p.text)  { $text  = $p.text  }
-    if ($p.time)  { $time  = $p.time  }
+    if ($p.title) { $title   = $p.title }
+    if ($p.text)  { $text    = $p.text  }
+    if ($p.time)  { $time    = $p.time  }
+    if ($p.icon)  { $iconUrl = $p.icon  }
 }
 
+$appId   = "HorrorGame.Notifier"
+$appName = "HorrorGame"
+$regPath = "HKCU:\SOFTWARE\Classes\AppUserModelId\$appId"
+
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+    New-ItemProperty -Path $regPath -Name "DisplayName"       -Value $appName -PropertyType String -Force | Out-Null
+    New-ItemProperty -Path $regPath -Name "ShowInSettings"    -Value 1        -PropertyType DWord  -Force | Out-Null
+    New-ItemProperty -Path $regPath -Name "AllowContentAboveLock" -Value 1   -PropertyType DWord  -Force | Out-Null
+}
+
+$tempImg = $null
+
 try {
-    Add-Type -AssemblyName System.Windows.Forms
     Add-Type -AssemblyName System.Drawing
 
-    $notification = New-Object System.Windows.Forms.NotifyIcon
-    $notification.Icon = [System.Drawing.SystemIcons]::Application
-    $notification.BalloonTipTitle = $title
-    $notification.BalloonTipText = $text
-    $notification.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::None
-    $notification.Visible = $true
-    $notification.ShowBalloonTip($time * 1000)
+    if ($iconUrl) {
+        $tempImg = Join-Path $env:TEMP "notif_icon_$(Get-Random).png"
+        try {
+            Invoke-WebRequest -Uri $iconUrl -OutFile $tempImg -UseBasicParsing
+        } catch {
+            $tempImg = $null
+        }
+    }
+
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType=WindowsRuntime] | Out-Null
+    [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType=WindowsRuntime] | Out-Null
+
+    if ($tempImg -and (Test-Path $tempImg)) {
+        $imgUri = "file:///" + $tempImg.Replace("\", "/")
+        $xml = @"
+<toast duration="long">
+  <visual>
+    <binding template="ToastGeneric">
+      <text>$title</text>
+      <text>$text</text>
+      <image placement="appLogoOverride" hint-crop="circle" src="$imgUri"/>
+    </binding>
+  </visual>
+</toast>
+"@
+    } else {
+        $xml = @"
+<toast duration="long">
+  <visual>
+    <binding template="ToastGeneric">
+      <text>$title</text>
+      <text>$text</text>
+    </binding>
+  </visual>
+</toast>
+"@
+    }
+
+    $xmlDoc = New-Object Windows.Data.Xml.Dom.XmlDocument
+    $xmlDoc.LoadXml($xml)
+
+    $toast = New-Object Windows.UI.Notifications.ToastNotification($xmlDoc)
 
     $global:clicked = $false
-    $clickAction = Register-ObjectEvent -InputObject $notification -EventName "BalloonTipClicked" -Action {
-        $global:clicked = $true
-    }
+    $toast.add_Activated({ $global:clicked = $true })
+
+    $notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($appId)
+    $notifier.Show($toast)
 
     $startTime = Get-Date
     while ((((Get-Date) - $startTime).TotalSeconds -lt $time) -and (-not $global:clicked)) {
@@ -40,15 +90,12 @@ try {
     if ($global:clicked -and ($null -ne $p) -and $p.connectionId) {
         @{
             connectionId = $p.connectionId
-            answer = "clicked"
+            answer       = "clicked"
         } | ConvertTo-Json -Compress | Set-Content "$PSScriptRoot\..\response_$($p.connectionId).json" -Encoding UTF8
     }
 
-    Unregister-Event -SourceIdentifier $clickAction.Name -ErrorAction SilentlyContinue
 } catch {}
 
 Start-Sleep -Seconds 1
-if ($null -ne $notification) {
-    $notification.Visible = $false
-    $notification.Dispose()
-}
+
+if ($tempImg -and (Test-Path $tempImg)) { Remove-Item $tempImg -Force -ErrorAction SilentlyContinue }
